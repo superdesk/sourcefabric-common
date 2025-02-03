1148faedfc9f7dcdb9b6b5f9f7a1a673d400fc4c
Tomas Kikutis <t.kikutis@gmail.com>
2025-02-03 15:18:24 +0100

    tooltip - wrap popper directly

--- src/ui/tooltip.stories.tsx
+++ src/ui/tooltip.stories.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import type {Meta, StoryObj} from '@storybook/react';
 import {WithSortable} from './with-sortable';
 import {Tooltip} from './tooltip';
+import {Placement} from 'tippy.js';
 
 const meta: Meta<typeof Tooltip> = {
     title: 'HOC/Tooltip',
@@ -14,11 +15,22 @@ type Story = StoryObj<typeof WithSortable>;
 
 export const Main: Story = {
     render: () => {
+        const placements: Array<Placement> = [
+            'top',
+            'bottom',
+            'right',
+            'left',
+        ]
+
         return (
-            <div>
-                <Tooltip content={'test tooltip'}>
-                    <div style={{width: 30, height: 30, background: 'red'}} />
-                </Tooltip>
+            <div style={{display: 'flex', gap: 10, padding: 100}}>
+                {
+                    placements.map((placement) => (
+                        <Tooltip content='The quick brown fox jumps over the lazy dog' placement={placement}>
+                            <div style={{padding: 4, border: '1px solid red'}}>{placement}</div>
+                        </Tooltip>
+                    ))
+                }
             </div>
         );
     },
--- src/ui/tooltip.tsx
+++ src/ui/tooltip.tsx
@@ -1,23 +1,66 @@
 import * as React from 'react';
-import Tippy from '@tippyjs/react';
-import {Placement} from 'tippy.js';
+import tippy, {Instance, Placement} from 'tippy.js';
 
 interface IProps {
-    content: string | null;
+    content: string | undefined | null;
     children: React.ReactNode;
     placement?: Placement;
     disabled?: boolean;
 }
 
 export class Tooltip extends React.PureComponent<IProps> {
-    render() {
-        const {content, children, placement} = this.props;
-        const disabled = this.props.disabled ?? false;
+    private id: string;
+    private instance: Instance | null;
 
+    constructor(props: IProps) {
+        super(props);
+
+        this.id = 'tooltip-' + Math.random().toString().slice(2);
+        this.instance = null;
+    }
+
+    private setupTooltip() {
+        if (this.instance == null) {
+            this.instance = tippy('#' + this.id, {
+                placement: this.props.placement,
+            })[0];
+
+            if (this.props.content != null) {
+                this.instance.setContent(this.props.content);
+            } else {
+                this.instance.hide();
+                this.instance.disable();
+            }
+        }
+
+        const willBeEnabled = this.props.content != null && this.props.disabled !== true;
+        const isEnabled = this.instance.state.isEnabled;
+
+        if (isEnabled && willBeEnabled) {
+            this.instance.setContent(this.props.content);
+        } else if (isEnabled) { // enabled now, but needs to be disabled
+            this.instance.hide();
+            this.instance.disable();
+        } else if (willBeEnabled) { // disabled now, but needs to be enabled
+            this.instance.setContent(this.props.content);
+            this.instance.enable();
+            this.instance.show();
+        }
+    }
+
+    componentDidMount(): void {
+        this.setupTooltip();
+    }
+
+    componentDidUpdate(): void {
+        this.setupTooltip();
+    }
+
+    render() {
         return (
-            <Tippy content={disabled ? null : content} placement={placement}>
-                {children as React.ReactElement}
-            </Tippy>
+            <div id={this.id} style={{display: 'inline-flex'}}>
+                {this.props.children}
+            </div>
         );
     }
 }
