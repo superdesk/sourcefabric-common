2dbcea0202b0c06b0c7c4eebe87cf94c75560c48
Tomas Kikutis <t.kikutis@gmail.com>
2025-03-14 10:19:03 +0100

    use locale for weekday names

--- src/localization/get-weekday-names.ts
+++ src/localization/get-weekday-names.ts
@@ -6,35 +6,36 @@ import {arraySpinBackwards} from '../utils/array-spin';
 export function getWeekdayNames(
     length: 'short' | 'long',
     firstDayOfWeek: number,
+    localeName: string,
 ): Array<{index: number; label: string}> {
     const all = [
         {
             index: 0,
-            label: new Intl.DateTimeFormat('en-us', {weekday: length}).format(new Date('1970-01-05')),
+            label: new Intl.DateTimeFormat(localeName, {weekday: length}).format(new Date('1970-01-05')),
         }, // Mon
         {
             index: 1,
-            label: new Intl.DateTimeFormat('en-us', {weekday: length}).format(new Date('1970-01-06')),
+            label: new Intl.DateTimeFormat(localeName, {weekday: length}).format(new Date('1970-01-06')),
         }, // Tue
         {
             index: 2,
-            label: new Intl.DateTimeFormat('en-us', {weekday: length}).format(new Date('1970-01-07')),
+            label: new Intl.DateTimeFormat(localeName, {weekday: length}).format(new Date('1970-01-07')),
         }, // Wed
         {
             index: 3,
-            label: new Intl.DateTimeFormat('en-us', {weekday: length}).format(new Date('1970-01-08')),
+            label: new Intl.DateTimeFormat(localeName, {weekday: length}).format(new Date('1970-01-08')),
         }, // Thu
         {
             index: 4,
-            label: new Intl.DateTimeFormat('en-us', {weekday: length}).format(new Date('1970-01-09')),
+            label: new Intl.DateTimeFormat(localeName, {weekday: length}).format(new Date('1970-01-09')),
         }, // Fri
         {
             index: 5,
-            label: new Intl.DateTimeFormat('en-us', {weekday: length}).format(new Date('1970-01-10')),
+            label: new Intl.DateTimeFormat(localeName, {weekday: length}).format(new Date('1970-01-10')),
         }, // Sat
         {
             index: 6,
-            label: new Intl.DateTimeFormat('en-us', {weekday: length}).format(new Date('1970-01-11')),
+            label: new Intl.DateTimeFormat(localeName, {weekday: length}).format(new Date('1970-01-11')),
         }, // Sun
     ];
 
