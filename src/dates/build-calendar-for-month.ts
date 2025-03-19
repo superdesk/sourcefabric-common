48bd60e19eb8aaa077073550b5da871629c471f6
Tomas Kikutis <t.kikutis@gmail.com>
2025-03-19 14:13:06 +0100

    fix month name

--- src/dates/build-calendar-for-month.ts
+++ src/dates/build-calendar-for-month.ts
@@ -32,7 +32,7 @@ export function buildCalendarForMonth(month: Date, locale: {code: string; weekSt
 
     return {
         weeks,
-        monthName: getMonthNames(locale.code, 'long')[getMonth(new Date())],
+        monthName: getMonthNames(locale.code, 'long')[getMonth(month)],
         weekdays: getWeekdayNames('short', weekStartsOn, locale.code).map(({label}) => label),
     };
 }
